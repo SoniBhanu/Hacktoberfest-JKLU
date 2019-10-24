3d12c2463897d87619d4727df3a2f19fd9c467ae
Immortal-Dhruv <56475164+Immortal-Dhruv@users.noreply.github.com>
2019-10-24 13:03:15 +0530

    Update bhanu.py

--- bhanu.py
+++ bhanu.py
@@ -1,4 +1,5 @@
 name="Bhanu"
 year="First"
 branch="Computer Science"
-print("Hello! I am "+name+" from "+year+" year "+branch+" and today's Hactoberfest event was fun!!!!  ")
+hobbies="Singing"
+print("Hello! I am "+name+" from "+year+" year "+branch+" hobbies "+hobbies+" and today's Hactoberfest event was fun!!!!  ")
