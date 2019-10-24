af8aa7166fdfa60e69b66186b42c26d57f6bc9bf
SoniKraken <56475004+SoniKraken@users.noreply.github.com>
2019-10-24 13:01:39 +0530

    Update dhruv.py

--- dhruv.py
+++ dhruv.py
@@ -1,4 +1,5 @@
 name="Dhruv"
 year="First"
 branch="Computer Science"
+skills="Playing Mini Militia"
 print("Hello! I am "+name+" from "+year+" year "+branch+" and today's Hactoberfest event was fun!!!!  ")
